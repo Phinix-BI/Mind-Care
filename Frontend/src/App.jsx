18a2b4ef0e347b8d9d86d289374babb2b221672b
Rupankar Das <rupankardasofficial@gmail.com>
2024-01-25 16:29:21 +0530

    Add Diagnostic Test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
 
 
+import "./index.css";
+
 import ProfilePage from "./components/ProfilePage/ProfilePage"
 import HomePage from "./components/HomePage/HomePage"
+import HeroSection from "./components/HeroSection/HeroSection"
 import Signup_Login_Form from "./components/Signup_Login_Form/Signup_Login_Form"
+import Diagnostic from "./components/Diagnostic/Diagnostic";
+import MCQ from "./components/MCQ/MCQ"
 
 const router = createBrowserRouter([
   {
@@ -19,6 +24,14 @@ const router = createBrowserRouter([
   {
     path: "/Signup_Login_Form",
     element:<Signup_Login_Form/>,
+  },
+  {
+    path: "/Diagnos",
+    element:<Diagnostic/>,
+  },
+  {
+    path: "/DiagnosTest",
+    element:<MCQ/>,
   }
  
 ]);
---
+++ Frontend/src/components/Diagnostic/Diagnostic.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Expertise from '../Expertise/Expertise'
+import Footer from '../common/Footer/Footer'
+import HeroSection from '../HeroSection/HeroSection'
+import NavBar from '../common/Navbar/Navbar'
+
+const Diagnostic = () => {
+  return (
+    <div>
+      
+      <NavBar />
+      <HeroSection />
+      <div className='max-w-screen-xl mx-auto'>
+
+      {/* Our Expertise */}
+      <div className='flex justify-center'>
+        <h1 className='text-4xl font-bold'>Our Expertise</h1>
+      </div>
+      
+      <div className='flex justify-center'>
+         <Expertise /><Expertise />
+        </div>
+        <div className='flex justify-center'>
+         <Expertise /> <Expertise />
+        </div>
+
+        
+      </div>
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default Diagnostic
