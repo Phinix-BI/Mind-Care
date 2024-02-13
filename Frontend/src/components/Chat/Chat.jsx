4acef6ad21cf18aa606345ad0e1fee09a443a9ba
Rupankar Das <rupankardasofficial@gmail.com>
2024-02-13 22:10:25 +0530

    Update Chat Frontend

--- Frontend/src/components/Chat/Chat.jsx
+++ Frontend/src/components/Chat/Chat.jsx
@@ -6,32 +6,36 @@ import TypingBox from '../TypingBox/TypingBox';
 
 const Chat = () => {
   return (
-    <div className='flex w-100vh'> {/* Apply background color to the entire chat interface */}
+    <div className='flex h-100vh'> {/* Apply background color to the entire chat interface */}
       {/* <NavBar /> Render the NavBar component */}
       <div className='max-w-screen-xl mx-auto'>
         {/* Welcome Note */}
         <div className='text-center p-4'>
             <h1 className='text-3xl p-4 font-semibold'>Welcome to your AI Therapy Session</h1>
-            <p className='text-lg font-medium text-gray-500 border-2 rounded-xl p-2'>I'm here to provide support and assistance as you navigate through life's 
+            <p className='text-lg font-medium text-gray-500 border-2 rounded-xl p-2 shadow-md'>I'm here to provide support and assistance as you navigate through life's 
             challenges. Whether you're seeking guidance, a listening ear, or simply a space to express yourself, 
             I'm here to help. Please feel free to share whatever is on your mind, and together, we can work
              towards understanding and overcoming any obstacles you may be facing.</p>
         </div>
         {/* 3D Avatar */}
-        <div className='m-10'>
+        <div className='m-10 float-center'>
             <img src="images\casual-life-3d-female-doctor-at-desk.png" className='mx-auto w-lg'/>
         </div>
         {/* Messages */}
-        <div>
+        <div className=''>
           <MessageAI />
           <MessageUser />
           <MessageAI />
           <MessageUser />
+          <MessageAI /><MessageAI />
+          <MessageUser />
+          <MessageAI />
+          <MessageUser />
+          <MessageAI /><MessageAI />
+          <MessageUser />
           <MessageAI />
           <MessageUser />
           <MessageAI />
-          <MessageAI />
-          <MessageUser /><MessageUser />
         </div>
         <TypingBox />
       </div>
