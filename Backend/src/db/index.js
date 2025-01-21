f7e9b43ab94da72445f38df439869ed3177e9839
Phinix-BI <biswajitdey.one@gmail.com>
2025-01-21 17:36:17 +0530

    Update nodemon to version 3.1.9 and add start/dev scripts in package.json; fix MongoDB connection string in db index

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,8 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`)
     console.log(`MongoDB Connected: ${connectionInstance.connection.host}`)
 }catch(error){
     console.log(`MongoDB Connection Error: ${error.message}`);
