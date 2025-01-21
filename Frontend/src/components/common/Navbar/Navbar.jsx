31ac31605d7e380c3ea9a512fd34c18a4a245fb9
Phinix-BI <biswajitdey.one@gmail.com>
2025-01-21 20:06:23 +0530

    Add useEffect import to Navbar component

--- Frontend/src/components/common/Navbar/Navbar.jsx
+++ Frontend/src/components/common/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
